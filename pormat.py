47d1eb6d0594413ed984f5667baf1934b3e0fbb5
Mas Budi <masbudicahngawi@gmail.com>
2024-01-29 21:06:05 +0700

    tambah file python

---
+++ pormat.py
@@ -0,0 +1,19 @@
+jawa = 124_346_481
+semua = 210_000_000
+
+perjawa = (jawa/semua)*100
+
+print(f"Persentase org Jawa : {perjawa:.3f}%")
+
+print("‐--------------‐------------------------")
+
+import locale
+
+duitku = 23450311842
+
+print(f"duitku : Rp {duitku:,.2f}")
+
+print("‐--------------‐------------------------")
+
+for i in range(10,25):
+    print(f'{i:04d}')
